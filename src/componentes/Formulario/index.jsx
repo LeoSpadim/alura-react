367b46a540825909f59015b331d2aaeb8611c46f
LeoSpadim <leospadim_new@outlook.com>
2023-03-02 23:24:27 -0300

    org and employees button

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import {HiOutlineViewGridAdd} from 'react-icons/hi';
 import Botao from '../Botao';
 import CampoLista from '../CampoLista';
 import CampoTexto from '../CampoTexto';
@@ -26,40 +27,47 @@ const Formulario = (props) =>{
 
     return(
         <section className="formulario">
-            <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card do desenvolvedor</h2>
-                <CampoTexto 
-                    label="Nome" 
-                    placeholder="Digite seu nome"
-                    obrigatorio={true}
+            <form className="employees_form" onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do desenvolvedor.</h2>
+                <HiOutlineViewGridAdd className='adicionar_colaborador' size={32}/>
+                <div>
+                    <CampoTexto 
+                        label="Nome" 
+                        placeholder="Digite seu nome"
+                        obrigatorio={true}
 
-                    value={nome}
-                    aoAlterar={valor => setNome(valor)}
-                />
-                <CampoTexto 
-                    label="Função"
-                    placeholder="Digite sua função"
-                    obrigatorio={true}
+                        value={nome}
+                        aoAlterar={valor => setNome(valor)}
+                    />
+                    <CampoTexto 
+                        label="Função"
+                        placeholder="Digite sua função"
+                        obrigatorio={true}
 
-                    value={cargo}
-                    aoAlterar={valor => setCargo(valor)}
-                />
-                <CampoTexto 
-                    label="Imagem"
-                    placeholder="Digite o endereço da imagem"
+                        value={cargo}
+                        aoAlterar={valor => setCargo(valor)}
+                    />
+                    <CampoTexto 
+                        label="Imagem"
+                        placeholder="Digite o endereço da imagem"
 
-                    value={imagem}
-                    aoAlterar={valor => setImagem(valor)}
-                />
-                <CampoLista 
-                    label="Time"
-                    itens={props.nomesTimes}
-                    obrigatorio={true}
+                        value={imagem}
+                        aoAlterar={valor => setImagem(valor)}
+                    />
+                    <CampoLista 
+                        label="Time"
+                        itens={props.nomesTimes}
+                        obrigatorio={true}
 
-                    value={time}
-                    aoAlterar={valor => setTime(valor)}
-                />
-                <Botao>Criar card</Botao>
+                        value={time}
+                        aoAlterar={valor => setTime(valor)}
+                    />
+                    <Botao>Criar card</Botao>
+                </div>
+            </form>
+            <form className="teams_form">
+                <h2>Adicionar times:</h2>
+                <HiOutlineViewGridAdd className='adicionar_time' size={32}/>
             </form>
         </section>
     );
